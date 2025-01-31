645cf3388c2200c17c4624394e0bad8a1ee5d28b
Tcquaranta <thiagocine40@hotmail.com>
2025-01-31 20:13:07 +0000

    adiciona scripts `lint: check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home(){
-    return <h1>Oi Mari, fica calminhaaaaaaaaa, apesar da tpm te amo lindona!</h1>
+function Home() {
+  return <h1>Oi Mari, fica calminhaaaaaaaaa, apesar da tpm te amo lindona!</h1>;
+}
+function teste() {
+  console.log("teste");
 }
 
+function teste2() {
+  console.log("vai se deeeeeeeeeeeee");
+}
 export default Home;
