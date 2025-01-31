b078251c87ff607a9c36bc0fee99584e6b5d3094
Tcquaranta <tcquaranta@outlook.com>
2025-01-31 19:24:12 +0000

    teste

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Oi Mari, fica calminhaaaaa, apesar da tpm te amo lindona!</h1>
+    return <h1>Oi Mari, fica calminhaaaaaaaaa agora, apesar da tpm te amo lindona!</h1>
 }
 
 export default Home;
